f242deed53a7b7ff949aea6666bfd6c0c6dd5407
Athanasios Boutsikakis <thanasis.boutsikakis@corintis.com>
2024-04-10 23:06:42 +0200

    Informed setup.py of the README

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+"""Setup script for the pytest-proceed package."""
+
 from setuptools import find_packages, setup
+
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="pytest-proceed",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
@@ -14,4 +20,6 @@ setup(
         "argparse",  # This is included in the standard library from Python 2.7 and 3.2 onwards, so it's generally not needed unless you support older versions.
     ],
     python_requires=">=3.6",  # Adjust based on the Python versions you want to support.
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
